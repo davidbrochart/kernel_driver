4759c829f072b22d417ccfb4fff316af762cd315
David Brochart <david.brochart@gmail.com>
2021-04-02 11:39:51 +0200

    v0.0.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="kernel_driver",
-    version="0.0.3",
+    version="0.0.4",
     url="https://github.com/davidbrochart/kernel_driver.git",
     author="David Brochart",
     author_email="david.brochart@gmail.com",
